ee63ab1b379868163ec5e5ccd44d3edee0d2e478
zjli_QMjYO <zjli@justhealth.cn>
2023-09-02 11:15:13 +0800

    修改代码

--- template_message/Iciba.py
+++ template_message/Iciba.py
@@ -108,7 +108,7 @@ class iciba:
         #     openid = openid.strip()
         #     result = self.send_msg(openid, self.template_id, everyday_words)
         #     self.print_log(result, openid)
-        index_number = 5
+        index_number = 91
         index = index_number
         for openid in openids[index_number:]:
             openid = openid.strip()
