9304dbe3b40398dc32f418dc67a29589d1e724b6
zjli_QMjYO <zjli@justhealth.cn>
2023-09-17 05:51:21 +0800

    修改端口号

--- main.py
+++ main.py
@@ -155,4 +155,4 @@ async def create_chat_completion(session, chat_model):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5678)
+    uvicorn.run(app, host="0.0.0.0", port=3344)
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 mkdir -p /home/xiangli/spiritual-cultivation_log
 
 # start chat server
-pid=$(lsof -ti :5678) && if [ ! -z "$pid" ]; then kill $pid; fi
+pid=$(lsof -ti :3344) && if [ ! -z "$pid" ]; then kill $pid; fi
 sleep 2
 nohup python3 main.py  > /home/xiangli/spiritual-cultivation_log/log_dev.txt 2>&1 &
 
