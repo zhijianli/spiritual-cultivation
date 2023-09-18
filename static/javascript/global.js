9a1635fb0dd2ba524aa0095e105c4007bd15e39a
zjli_QMjYO <zjli@justhealth.cn>
2023-09-18 17:01:46 +0800

    增加模型微调代码

--- static/javascript/global.js
+++ static/javascript/global.js
@@ -1,2 +1,2 @@
-var socket_address = 'ws://127.0.0.1:7777/ws';
-//var socket_address = 'ws://ai.menganhealth.cn:7777/ws';
+//var socket_address = 'ws://127.0.0.1:7777/ws';
+var socket_address = 'ws://ai.menganhealth.cn:7777/ws';
