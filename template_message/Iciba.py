8d8aa9c7ad25e7cccbe4579f22b9dd0c02db64a2
zjli_QMjYO <zjli@justhealth.cn>
2023-10-24 21:23:40 +0800

    修改代码

--- template_message/Iciba.py
+++ template_message/Iciba.py
@@ -108,7 +108,7 @@ class iciba:
         #     openid = openid.strip()
         #     result = self.send_msg(openid, self.template_id, everyday_words)
         #     self.print_log(result, openid)
-        index_number = parameter
+        index_number = int(parameter)
         print(f"你传入的参数是: {index_number}")
         index = index_number
         for openid in openids[index_number:]:
